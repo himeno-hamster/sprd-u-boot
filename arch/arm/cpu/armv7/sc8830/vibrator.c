444e3fae48a633b26f887b7151fb5b94adca226e
shan.han <shan.han@spreadtrum.com.cn>
2013-04-23 15:51:44 +0800

    update vibrator driver for sc8830
    Change-Id: Ibcbed7a11cd730de64ef7811092df988566c09a2

--- arch/arm/cpu/armv7/sc8830/vibrator.c
+++ arch/arm/cpu/armv7/sc8830/vibrator.c
@@ -8,7 +8,7 @@
 #include <asm/arch/sprd_reg.h>
 //#include <asm/arch/analog_reg_v3.h>
 
-#define VIBRATOR_REG_UNLOCK (0xA1B2)
+#define VIBRATOR_REG_UNLOCK (0x1A2B)
 #define VIBRATOR_REG_LOCK   (~VIBRATOR_REG_UNLOCK)
 #define VIBRATOR_STABLE_LEVEL   (4)
 #define VIBRATOR_INIT_LEVEL (11)    //init level must larger than stable level
@@ -42,7 +42,7 @@ void set_vibrator(int on)
 void vibrator_hw_init(void)
 {
 	ANA_REG_SET(ANA_REG_GLB_VIBR_WR_PROT_VALUE, VIBRATOR_REG_UNLOCK); //unlock vibrator registor
-
+	ANA_REG_OR(ANA_REG_GLB_RTC_CLK_EN, BIT_RTC_VIBR_EN);
 	ANA_REG_MSK_OR(ANA_REG_GLB_VIBR_CTRL0, (VIBRATOR_INIT_LEVEL << VIBR_INIT_V_SHIFT), VIBR_INIT_V_MSK); //set init current level
 	ANA_REG_MSK_OR(ANA_REG_GLB_VIBR_CTRL0, (VIBRATOR_STABLE_LEVEL << VIBR_STABLE_V_SHIFT), VIBR_STABLE_V_MSK); //set stable current level
 	ANA_REG_SET(ANA_REG_GLB_VIBR_CTRL1, VIBRATOR_INIT_STATE_CNT);   //set convert count
