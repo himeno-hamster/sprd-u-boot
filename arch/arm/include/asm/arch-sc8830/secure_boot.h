82e69599db49181338d900806ec5f025a83797ff
Tim Luo <tim.luo@spreadtrum.com>
2013-05-15 18:20:27 +0800

    Revert "add security function in spl for sc8830"
    This reverts commit 7be0c98ba9639d092e38fa65f9f69d47a1042645

--- arch/arm/include/asm/arch-sc8830/secure_boot.h
+++ arch/arm/include/asm/arch-sc8830/secure_boot.h
@@ -6,7 +6,6 @@
 #define CUSTOM_DATA_SIZ (1024)
 #define VLR_INFO_SIZ (512)
 #define VLR_INFO_OFF (512)
-#define INTER_RAM_BEGIN 0x50005000
 
 void secure_check(uint8_t *data, uint32_t data_len, uint8_t *data_hash, uint8_t *data_key);
 int cal_md5(void *data, uint32_t len, void *harsh_data);
