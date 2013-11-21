67967cd51fe15464433be831bee0e815c484c61d
hua.fang <hua.fang@spreadtrum.com>
2013-11-21 19:26:40 +0800

    Bug#239343 - Device Boots Up Automatically on connecting battery
    	make it normal boot when insert jig box
    
    Change-Id: Icf335d7b0ad030f0a8fa19c14df2feb45412d74b

--- property/cmd_cboot.c
+++ property/cmd_cboot.c
@@ -169,6 +169,12 @@ int do_cboot(cmd_tbl_t *cmdtp, int flag, int argc, char *const argv[])
             charge_mode();
         }
     }
+#if defined(CONFIG_SP8830SSW)
+	else if (1)
+	{
+		;
+	}
+#endif
     else{
         DBG("cboot:get mode fail , and shutdown device\n");
         power_down_devices();
