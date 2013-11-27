209161295a799b615200f6ed669c00bab7809b4b
lunping.guo <lunping.guo@spreadtrum.com>
2013-11-27 14:07:50 +0800

    Bug#244826  add mcp mvr*658(K524G2GACJ-B050) configuration [root cause ] MVR*658 has been verified [changes ]
    Change-Id: Ie4fcfa2c144dbf945cf62e24d7847dd8197fd46b

--- arch/arm/cpu/armv7/sc8810/sdram_prod_sc7710g2.c
+++ arch/arm/cpu/armv7/sc8810/sdram_prod_sc7710g2.c
@@ -45,6 +45,7 @@ LOCAL CONST EMC_PARAM_T s_emc_parm[] =
 	//{CHIP_CLK_1000MHZ, EMC_CLK_400MHZ, DDR_DRV_STR_TR_Q,        2,		2,          1,      2,          10,			{0xad, 0xbc, 0x90, 0x55, 0x56}},  // H9DA4GH2GJBMCR, just example, scan your board then modify para
       {CHIP_CLK_1000MHZ, EMC_CLK_333MHZ, DDR_DRV_STR_TR_Q,        1,          2,          1,      2,          15,                     {0xec, 0xbc, 0x01, 0x55, 0x48}},  // KBY00N00HM-A448, just example ,scan your board then modify para
 
+      //{CHIP_CLK_1000MHZ, EMC_CLK_400MHZ, DDR_DRV_STR_TR_Q,        1,          2,          2,      1,          11,                     {0xec, 0xbc, 0x00, 0x66, 0x56}},  // K524G2GACJ-B050, mvr658, just example ,scan your board then modify para
 
   //{CHIP_CLK_1000MHZ, EMC_CLK_400MHZ, DDR_DRV_STR_TR_Q,		1,		2,			1,		2,			12, 		{0x2c, 0xb3, 0x90, 0x66, 0x64}},  // custom para, just example
 
